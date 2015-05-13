e5b14b3d185937817800741a366aa09e18eefde0
EleanorK <eleanorkwd@gmail.com>
2015-05-13 03:38:46 -0400

    pizza!

--- mysite/FindTeammates/urls.py
+++ mysite/FindTeammates/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.roster, name='roster'),
+    
     url(r'^roster/$', views.roster, name='roster'),
     url(r'^teams/$', views.teams, name='teams'),
+    url(r'^ajax$', views.ajax, name='ajax'),
+    url(r'^test$', views.main),
+    url(r'^inviteHis$', views.updateInviteHistory),
 ]
--- mysite/FindTeammates/views.py
+++ mysite/FindTeammates/views.py
@@ -3,9 +3,62 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.shortcuts import render_to_response
 
+from django.http import *
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+import json
+import socket
+
+def LinkedIn_login(request):
+	'''
+	TODO:
+	get the user's ID
+	get the course ID
+	'''
+	context = RequestContext(request, {'request': request, 'user': request.user})
+    return render_to_response('FindTeammates/login.html',context_instance=context)
+
+
 
 def roster(request):
-	return render_to_response('FindTeammates/roster.html')
+	return render_to_response('FindTeammates/roster.html', context_instance=RequestContext(request))
 
 def teams(request):
 	return render_to_response('FindTeammates/teams.html')
+
+def main(request):
+	return render_to_response('FindTeammates/test.html', context_instance=RequestContext(request))
+
+def updateInviteHistory(request):
+
+	if request.POST.has_key('client_response'):
+		print 'shit'
+		x = request.POST['client_response']
+		'''
+		TODO:
+		update invite history in database
+		'''
+		print x
+		
+		return render_to_response('FindTeammates/roster.html', context_instance=RequestContext(request))
+	else:
+		return render_to_response('FindTeammates/roster.html', context_instance=RequestContext(request))
+
+def updateJoinHistory(request):
+
+	if request.POST.has_key('client_response'):
+		print 'shit'
+		x = request.POST['client_response']
+		'''
+		TODO:
+		update join history in database
+		'''
+		print x
+		
+		return render_to_response('FindTeammates/teams.html', context_instance=RequestContext(request))
+	else:
+		return render_to_response('FindTeammates/teams.html', context_instance=RequestContext(request))
+
+
+
+
